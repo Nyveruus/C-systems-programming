e996bfefac75b9459fefd549c5d75063db672bb9
Nyveruus <106652476+Nyveruus@users.noreply.github.com>
2026-04-17 18:35:46 +0200

    Setup SSL object

--- projects/networking/mtls-chat/server.c
+++ projects/networking/mtls-chat/server.c
@@ -1,4 +1,23 @@
+//listen for inocming connections poll, complete tcp handshake, tls handshake, poll, read
+//create ssl object
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <openssl/ssl.h>
+
+#include "server.h"
 
 int server(int port, char *ca, char *cert, char *key) {
+    SSL_CTX *ctx;
+    setup_object(&ctx);
+
+    SSL_CTX_free(ctx);
     return 0;
+}
+
+void setup_object(SSL_CTX **ctx) {
+    if (!(*ctx = SSL_CTX_new(TLS_server_method()))) {
+        perror("SSL_CTX_new");
+        exit(1);
+    }
 }
--- projects/networking/mtls-chat/server.h
+++ projects/networking/mtls-chat/server.h
@@ -1,4 +1,8 @@
 #ifndef SERVER_H
 #define SERVER_H
+
+#include <openssl/ssl.h>
 int server(int port, char *ca, char *cert, char *key);
+void setup_object(SSL_CTX *object);
+
 #endif
