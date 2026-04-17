074c0fcf83be50878062368acf0e8c3584cb8ab5
Nyveruus <106652476+Nyveruus@users.noreply.github.com>
2026-04-17 11:27:18 +0200

    Usage and select server or client in main

--- projects/networking/mtls-chat/main.c
+++ projects/networking/mtls-chat/main.c
@@ -1,1 +1,27 @@
-//
+//check if server or client in argument, call client or server functions respectively
+
+#include <stdio.h>
+#include <string.h>
+
+int select(int argc, char **argv) {
+    if (!strcmp(argv[1], "server"))
+        return server();
+    else if (!strcmp(argv[1], "client"))
+        return; //client function
+    else
+        usage();
+}
+
+void usage(void) {
+    fprintf(stderr, "Usage: ./mtlsapp server|client\n");
+    return;
+}
+
+int main (int argc, char *argv[]) {
+    if (argc < 2) {
+        usage();
+        return 1;
+    }
+    select(argc, argv);
+}
+
